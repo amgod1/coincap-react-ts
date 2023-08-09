145810ab7e7bf31393f0e2dfb2526c9f3d2b534e
amgod1 <kamillschepuk@gmail.com>
2023-08-09 14:57:41 +0300

    feat: table component + styles

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,38 @@
+import styles from "./Table.module.scss"
+
 const Table = () => {
-  return <div>Table</div>
+  return (
+    <table className={styles.table}>
+      <thead className={styles["table__head"]}>
+        <tr>
+          <td>Title</td>
+          <td>Price</td>
+          <td>Change (24Hr)</td>
+          <td>Add</td>
+        </tr>
+      </thead>
+      <tbody className={styles["table__body"]}>
+        <tr>
+          <td>Bitcoin</td>
+          <td>20b</td>
+          <td>1%</td>
+          <td>button</td>
+        </tr>
+        <tr>
+          <td>Bitcoin</td>
+          <td>20b</td>
+          <td>1%</td>
+          <td>button</td>
+        </tr>
+        <tr>
+          <td>Bitcoin</td>
+          <td>20b</td>
+          <td>1%</td>
+          <td>button</td>
+        </tr>
+      </tbody>
+    </table>
+  )
 }
 
 export default Table
